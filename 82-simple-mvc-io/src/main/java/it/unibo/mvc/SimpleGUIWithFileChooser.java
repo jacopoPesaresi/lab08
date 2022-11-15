5276a4c7e0d49312970be98d4da4c7257e27c472
Jacopo Pesaresi - jacopo.pesaresi2@studio.unibo.it <jacopo.pesaresi2@studio.unibo.it>
2022-11-15 17:26:42 +0100

    Esercice 82 done. Waiting for correction. Push work done so far

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,14 +1,122 @@
 package it.unibo.mvc;
 
-//import javax.swing.JFrame;
+//import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+//import javax.swing.JTextArea;
+import javax.swing.JTextField;
+//import javax.swing.border.Border;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
+    private Controller controller = new Controller();
+    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 3;
 
-    //private final JFrame frame = new JFrame();
+    /**
+     * 
+     */
+    public SimpleGUIWithFileChooser() {
 
+        frame.setTitle("My little GUI (with file chooser)");
+
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        final JPanel northG = new JPanel();
+        northG.setLayout(new BorderLayout());
+        final JTextField path = new JTextField();
+
+        path.setEditable(false);
+        path.setText(controller.getPathString());
+
+
+        final JButton browseB = new JButton("Browse...");
+        northG.add(path, BorderLayout.CENTER);
+        northG.add(browseB, BorderLayout.LINE_END);
+        canvas.add(northG, BorderLayout.NORTH);
+
+
+        final JButton saveB = new JButton("Save");
+        canvas.add(saveB, BorderLayout.SOUTH);
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /*
+        saveB.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+            }
+        });
+        */
+        browseB.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+               final JFileChooser fChooser = new JFileChooser();
+               if (fChooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                controller.setCurrentFile(fChooser.getSelectedFile());
+                path.setText(controller.getPathString());
+               } else {
+                JOptionPane.showMessageDialog(frame, "Nome file non trovato", "Error", JOptionPane.ERROR_MESSAGE);
+               }
+            }
+        });
+    }
+
+
+    /**
+     * 
+     */
+    public void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+
+        //frame.pack(); 
+
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(final String[] args) {
+        final SimpleGUIWithFileChooser g = new SimpleGUIWithFileChooser();
+        g.display();
+    }
 }
