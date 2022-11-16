481b3c0fe671e2f397ac77596c0c9b6ecb0e940c
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2022-11-16 17:21:07 +0100

    Star work at home (tecnicaly)

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,34 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * method that set the next string to print
+     * @param newString
+     * @throws Exception
+     */
+    void setNextString(final String newString) throws Exception;
+
+    /**
+     * method to get the next string to print
+     */
+    String getNextString();
+
+    /**
+     * get the history of the printed strings
+     * @return a List of String that is the history
+     */
+    List<String> getHistory();
+    
+    /**
+     * 
+     * @throws IllegalStateException if the current string is unset
+     */
+    void printActualString() throws IllegalStateException;
 
 }
--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,52 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+    private String myString;
+    private List<String> history;
+
+
+    public SimpleController(String firstString){
+        myString = firstString;
+        history = new ArrayList<>();
+    }
+
+    public SimpleController(){
+        this("");
+    }
+
+    @Override
+    public void setNextString(final String newString) throws Exception {
+        if (newString !=null){
+            myString = newString;
+        } else throw new Exception();
+        
+    }
+
+    @Override
+    public String getNextString() {
+        return new String(myString);
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return new ArrayList<String>(history);
+    }
+
+    @Override
+    public void printActualString() throws IllegalStateException {
+        if(myString!=null){
+            System.out.println(myString);
+        } else {
+            throw new IllegalStateException();
+        }
+        
+    }
 
 }
